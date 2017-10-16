d1496a51450419c073975f29d0641364f559818e
mikand13 <mikkelsen.anders@gmail.com>
2017-10-16 13:35:05 +0200

    updated version

--- src/main/java/com/nannoq/tools/auth/services/package-info.java
+++ src/main/java/com/nannoq/tools/auth/services/package-info.java
@@ -1,7 +1,7 @@
 /**
  * Created by anders on 28/12/2016.
  */
-@ModuleGen(name = "NannoqAuthServices", groupPackage="com.nannoq.tools.auth.services")
+@ModuleGen(name = "nannoqAuthServices", groupPackage="com.nannoq.tools.auth.services")
 package com.nannoq.tools.auth.services;
 
 import io.vertx.codegen.annotations.ModuleGen;
